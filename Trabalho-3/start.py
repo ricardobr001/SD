cb56878dbd8b83f17ceffe66eb40eeeaaa5a5e8b
Ricardo Mendes <ricardomlealjr@gmail.com>
2017-10-04 10:55:46 -0300

    Criando inicializador

--- Trabalho-3/start.py
+++ Trabalho-3/start.py
@@ -3,5 +3,5 @@ from subprocess import call
 call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25000 0'])
 call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25001 1'])
 call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25002 2'])
-call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25000 3'])
-call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25001 4'])
+call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25003 3'])
+call(['gnome-terminal' , '-e' ,'bash','--command','python Process.py 25004 4'])
